0ab4bd4a423cf68b17886a25184844d41ed2c14b
si3mshady <cloudwarrior2891@gmail.com>
2021-02-08 15:51:53 -0600

    updated getImage function

--- src/components/MediaCapture.js
+++ src/components/MediaCapture.js
@@ -2,19 +2,36 @@ import React from 'react'
 
 export default function MediaCapture() {
 
+    const loadedDOM = () => {
+        document.addEventListener('DOMContentLoaded', (e) => {
+            
+            const camera_input = document.getElementById("camera")
+            console.log('DOM loaded')
+            console.log('Added eventlistener')
+        
+        
+        
+        } )
+    }
+
     const getImage = (event) => {
         const camera_input = document.getElementById("camera")
         camera_input.addEventListener("change", (event) => {
-            console.dir(event)
-            console.log(event)
+            
+            console.log(event.target.files[0].name)
+            const image = document.getElementById("img")
+            image.src = window.URL.createObjectURL(event.target.files[0])
 
             
         } )
     }
     return (
        <>
-            <form action="#" id="form" enctype="multipart/form-data">
-                    <input onChange={getImage} type="file" id="camera"
+       {/* {loadedDOM()} */}
+            <form action="#" id="form" encType="multipart/form-data">
+                    
+
+                    <input onClick={getImage} placeholder="Open Camera" type="file" id="camera"
                     accept="image/*" capture />
                     <img src="" id="img" alt="camera phone" />
             </form>
